450a9688341ecdefc9b1e9bb75b6f1e17a4c258c
Terence Carroll <git@thack.com>
2014-05-22 14:44:08 -0700

    Defined as separate module and expanded example.

---
+++ sampleapp/app.js
@@ -0,0 +1,8 @@
+// setup application
+angular.module('sampleapp', ['matchInput']);
+
+// application controller
+angular.module('sampleapp').controller('AppController', function($scope) {
+    // application controller goes here
+    //$scope.password = 'secret';
+});
